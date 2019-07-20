7b42ffaf2032dbd09b38e1dee054fe0261cc86bd
anotherMonogatari <azaza270898@gmail.com>
2019-07-20 11:33:43 +0300

    added heal, strong attack, power and armor ups

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 const App = (props) => {
+  // Displaying stats while HP != 0 and fight result when HP < 0
     return <section>
       <div style={{textAlign: 'center'}}>
       {(props.you.hp > 0 && props.enemy.hp > 0) && <div>
         <p>You</p>
-        <p>HP: {props.you.hp}</p>
+        <p>HP: {props.you.hp} <span
+          style={(props.you.wound > 0) ? {color: 'red'} : {color: 'green'}}>{(props.you.wound !== 0) && -props.you.wound}</span></p>
+        <p>Armor: {props.you.armor}</p>
         <p>Attack: {props.you.power}</p>
         <p>Enemy</p>
-        <p>HP: {props.enemy.hp}</p>
+        <p>HP: {props.enemy.hp} <span 
+          style={{color: 'red'}}>{(props.enemy.wound !== 0) && -props.enemy.wound}</span></p>
+        <p>Armor: {props.enemy.armor}</p>
         <p>Attack: {props.enemy.power}</p>
 
         <button onClick={props.attackHandler}>Attack</button>
+        <button onClick={props.thunderHandler} disabled={(props.you.thunderCD <= 0) ? false : true}>Thunder</button>
+        <button onClick={props.armorHandler} disabled={(props.you.armorCD <= 0) ? false : true}>Armor Up</button>
+        <button onClick={props.healHandler} disabled={(props.you.healCD <= 0) ? false : true}>Heal up</button>
+        <button onClick={props.powerHandler} disabled={(props.you.powerCD <= 0) ? false : true}>Power Up</button>
       </div>}
      
       <button onClick={props.restartHandler}>Restart</button>
@@ -34,7 +43,11 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     attackHandler: () => dispatch({type: 'ATTACK'}),
-    restartHandler: () => dispatch({type: 'RESTART'})
+    restartHandler: () => dispatch({type: 'RESTART'}),
+    thunderHandler: () => dispatch({type: 'THUNDER'}),
+    armorHandler: () => dispatch({type: 'ARMOR'}),
+    healHandler: () => dispatch({type: 'HEAL'}),
+    powerHandler: () => dispatch({type: 'POWER'})
   }
 }
 
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,28 +1,139 @@
+// Characters stats
 const initState = {
     turn: 0,
     you: {
-      hp: 100,
-      power: 19
+      hp: 100000,
+      power: 100,
+      armor: 10,
+      wound: 0,
+      thunderCD: 0,
+      healCD: 0,
+      armorCD: 0,
+      powerCD: 0
     },
     enemy: {
-      hp: 150,
-      power: 7
+      hp: 100000,
+      power: 100,
+      armor: 40,
+      wound: 0
     }
   }
 
 const reducer = (state = initState, action) => {
+    // Randomazing attack
+    const minEnemyAttack = state.enemy.power * 0.9;
+    const maxEnemyAttack = state.enemy.power * 1.1;
+    const minYouAttack = state.you.power * 0.9;
+    const maxYouAttack = state.you.power * 1.1;
+    const attackRandom = (min, max) => {
+        return Math.round(Math.random() * (max - min + 1) ) + min;
+      }
+      // Calculating damage reduction by armor
+    let yourAttack = attackRandom(minYouAttack, maxYouAttack) - state.enemy.armor;
+    let enemyAttack = attackRandom(minEnemyAttack, maxEnemyAttack) - state.you.armor;
+    let yourHeal = attackRandom(minYouAttack, maxYouAttack);
+
     if (action.type === 'ATTACK') {
+          // Changing HP of characters and writing wounds they got
+          // attack decrease abilities CDs by 1 (if CD <= 0 you can use it again)
+          // when using some ability it also decrease CD of other abilities by 1
+            return {
+              turn: state.turn + 1,
+              you: {
+                ...state.you,
+              hp: state.you.hp - enemyAttack,
+              wound: enemyAttack,
+              thunderCD: state.you.thunderCD - 1,
+              healCD: state.you.healCD - 1,
+              powerCD: state.you.powerCD - 1,
+              armorCD: state.you.armorCD - 1
+            },
+              enemy: {
+                ...state.enemy,
+              hp: state.enemy.hp - yourAttack,
+              wound: yourAttack
+            }} 
+    }
+    // Thunder strike mechanic
+    let thunderAttack = yourAttack * 4
+
+    if (action.type === 'THUNDER') {
         return {
             turn: state.turn + 1,
             you: {
               ...state.you,
-            hp: state.you.hp - state.enemy.power
+            hp: state.you.hp - enemyAttack,
+            wound: enemyAttack,
+            thunderCD: 3,
+            healCD: state.you.healCD - 1,
+            powerCD: state.you.powerCD - 1,
+            armorCD: state.you.armorCD - 1
           },
             enemy: {
               ...state.enemy,
-            hp: state.enemy.hp - state.you.power
+            hp: state.enemy.hp - thunderAttack,
+            wound: thunderAttack
           }}
     }
+    // Healing mechanic
+    if (action.type === 'HEAL') {
+        return {
+            turn: state.turn + 1,
+            you: {
+              ...state.you,
+            hp: state.you.hp - enemyAttack + yourHeal*2,
+            wound: enemyAttack - yourHeal*2,
+            healCD: 4,
+            thunderCD: state.you.thunderCD - 1,
+            powerCD: state.you.powerCD - 1,
+            armorCD: state.you.armorCD - 1
+          },
+            enemy: {
+              ...state.enemy,
+            wound: 0
+          }}
+    }
+    // Armor up
+    if (action.type === 'ARMOR') {
+      const armorBonus = 40;
+      return {
+        turn: state.turn + 1,
+        you: {
+          ...state.you,
+        armor: state.you.armor + armorBonus,
+        hp: state.you.hp - enemyAttack + armorBonus,
+        wound: enemyAttack - armorBonus,
+        armorCD: 5,
+        thunderCD: state.you.thunderCD - 1,
+        powerCD: state.you.powerCD - 1,
+        healCD: state.you.healCD - 1
+      },
+        enemy: {
+          ...state.enemy,
+        wound: 0
+      }}
+    }
+    // power up
+    if (action.type === 'POWER') {
+      const powerBonus = 100;
+      return {
+        turn: state.turn + 1,
+        you: {
+          ...state.you,
+        hp: state.you.hp - enemyAttack,
+        wound: enemyAttack,
+        power: state.you.power + powerBonus,
+        powerCD: 5,
+        armorCD: state.you.armorCD - 1,
+        thunderCD: state.you.thunderCD - 1,
+        healCD: state.you.healCD - 1
+      },
+        enemy: {
+          ...state.enemy,
+        wound: 0
+      }}
+    }
+    // RESTART button will refresh page
     if (action.type === 'RESTART') {
         document.location.reload()
     }
